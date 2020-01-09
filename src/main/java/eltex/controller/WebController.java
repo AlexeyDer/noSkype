0bac5e4694f6facf466a5f0b249e82ae33842080
AlexeyDer <hottelmich@gmail.com>
2020-01-09 13:36:31 +0700

    Add start controller for next tasks

---
+++ src/main/java/eltex/controller/WebController.java
@@ -0,0 +1,19 @@
+package eltex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+class WebController {
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}

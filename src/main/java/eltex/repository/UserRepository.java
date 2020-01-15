68ac8a727f72555bc634556862986fd51815c093
AlexeyDer <hottelmich@gmail.com>
2020-01-15 16:48:03 +0700

    Add javaDoc

--- src/main/java/eltex/repository/UserRepository.java
+++ src/main/java/eltex/repository/UserRepository.java
@@ -3,6 +3,14 @@ package eltex.repository;
 import eltex.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Интерфейс для взаимодействия с сущностью.
+ * Можно прямо дополнить его своими методами запросов и выполнять операции.
+ *
+ * @author "Alexey Derevtsov"
+ * @version 1.0.0
+ */
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }

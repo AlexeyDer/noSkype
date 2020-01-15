93819e1af79aa07ab76e16094789cbdaf15e329f
AlexeyDer <hottelmich@gmail.com>
2020-01-15 16:42:09 +0700

    Add UserRepository

---
+++ src/main/java/eltex/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package eltex.repository;
+
+import eltex.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}

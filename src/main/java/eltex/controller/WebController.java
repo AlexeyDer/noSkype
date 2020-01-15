3ec18571aa7fa3285cdce876754783fe482020af
AlexeyDer <hottelmich@gmail.com>
2020-01-15 16:37:39 +0700

    Add javaDoc to MvcConfig

--- src/main/java/eltex/controller/WebController.java
+++ src/main/java/eltex/controller/WebController.java
@@ -1,52 +1,46 @@
 package eltex.controller;
 
 // import eltex.config.SpringConfig;
-import eltex.TestHibernate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import eltex.entity.Person;
-import eltex.service.PersonService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class WebController {
-    private PersonService personService;
-
-    @Autowired(required = true)
-    @Qualifier(value = "personService")
-    public void setPersonService(PersonService personService) {
-        this.personService = personService;
-    }
-
-    @RequestMapping("persondata/{id}")
-    public String personData(@PathVariable("id") int id, Model model){
-        model.addAttribute("person", this.personService.getPersonById(id));
-
-        return "person";
-    }
-
-    @RequestMapping(value = "/persons/add", method = RequestMethod.GET)
-    public String addPerson(Model model){
-        Person person1 = new Person(1, "lili", "900", "lilii");
-        personService.addPerson(person1);
-
-        return "person";
-    }
-
-    @RequestMapping(value = "person", method = RequestMethod.GET)
-    public String listPerson(Model model){
-        model.addAttribute("person", new Person());
-        model.addAttribute("listPerson", this.personService.listPersons());
-
-        return "person";
-    }
+//    private PersonService personService;
+//
+//    @Autowired(required = true)
+//    @Qualifier(value = "personService")
+//    public void setPersonService(PersonService personService) {
+//        this.personService = personService;
+//    }
+//
+//    @RequestMapping("persondata/{id}")
+//    public String personData(@PathVariable("id") int id, Model model){
+//        model.addAttribute("person", this.personService.getPersonById(id));
+//
+//        return "person";
+//    }
+//
+//    @RequestMapping(value = "/persons/add", method = RequestMethod.GET)
+//    public String addPerson(Model model){
+//        Person person1 = new Person(1, "lili", "900", "lilii");
+//        personService.addPerson(person1);
+//
+//        return "person";
+//    }
+//
+//    @RequestMapping(value = "person", method = RequestMethod.GET)
+//    public String listPerson(Model model){
+//        model.addAttribute("person", new Person());
+//        model.addAttribute("listPerson", this.personService.listPersons());
+//
+//        return "person";
+//    }
 
     @GetMapping("/")
     public String greeting(Model model) {
-        TestHibernate testHibernate = new TestHibernate();
         return "greeting";
     }
 

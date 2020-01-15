ac8ee8632274809f137c9e60c3b0a8b9b76d4aed
AlexeyDer <hottelmich@gmail.com>
2020-01-15 16:49:46 +0700

    Add enum Role

---
+++ src/main/java/eltex/entity/Role.java
@@ -0,0 +1,12 @@
+package eltex.entity;
+
+/**
+ *  Класс для перечесления возможных ролей пользователя.
+ *
+ *  @author "Alexey Derevtsov"
+ *  @version 1.0.0
+ *
+ */
+public enum Role {
+    USER;
+}

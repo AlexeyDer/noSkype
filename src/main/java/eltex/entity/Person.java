9187950b7b3a634b46ee96fc05534431b03a6175
AlexeyDer <hottelmich@gmail.com>
2020-01-15 16:41:15 +0700

    Commit Person on some time

--- src/main/java/eltex/entity/Person.java
+++ src/main/java/eltex/entity/Person.java
@@ -28,33 +28,33 @@ import java.util.List;
  *
  */
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class Person extends User {
-
-    private String login;
-    private String password; // следует хешировать
-    private String mail; // поле следует проверять на правильность
-    // private List<Person> friend;
-    private String status;
-    private String dateOfLastVisit;
-
-    public Person(Integer id, String fio, String phone, String login) {
-        super(id, fio, phone);
-        this.login = new String(login);
-        this.password = "none";
-        this.mail = "mail@mail.ru";
-        // this.friend = null;
-        this.status = "offline";
-        this.dateOfLastVisit = "today";
-    }
-
-    @Override
-    public String toString(){
-        return "[id = " + super.getId() + ", login = " + this.login + "]";
-    }
-
-}
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//public class Person extends User {
+//
+//    private String login;
+//    private String password; // следует хешировать
+//    private String mail; // поле следует проверять на правильность
+//    // private List<Person> friend;
+//    private String status;
+//    private String dateOfLastVisit;
+//
+//    public Person(Integer id, String fio, String phone, String login) {
+//        super(id, fio, phone);
+//        this.login = new String(login);
+//        this.password = "none";
+//        this.mail = "mail@mail.ru";
+//        // this.friend = null;
+//        this.status = "offline";
+//        this.dateOfLastVisit = "today";
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return "[id = " + super.getId() + ", login = " + this.login + "]";
+//    }
+//
+//}

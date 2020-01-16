6d59b59d5958237a1c60edade807806ad80f16f3
AlexeyDer <hottelmich@gmail.com>
2020-01-16 12:37:55 +0700

    Optimization imports

--- src/main/java/eltex/Application.java
+++ src/main/java/eltex/Application.java
@@ -2,7 +2,6 @@ package eltex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  *  Класс для запуска программы

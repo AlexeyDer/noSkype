0184ed8e6726a6cdb0d1c796a59606425c7218b9
AlexeyDer <hottelmich@gmail.com>
2020-01-16 15:58:16 +0700

    Add findById

--- src/main/java/eltex/repository/UserRepository.java
+++ src/main/java/eltex/repository/UserRepository.java
@@ -3,6 +3,8 @@ package eltex.repository;
 import eltex.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Интерфейс для взаимодействия с сущностью.
  * Можно прямо дополнить его своими методами запросов и выполнять операции.
@@ -13,4 +15,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+    User findById(Long id);
 }

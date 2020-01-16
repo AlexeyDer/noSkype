177d149de5250b7a7ed4c60d622ec29bf7d1716b
AlexeyDer <hottelmich@gmail.com>
2020-01-16 12:30:43 +0700

    Add UserService for Transaction

---
+++ src/main/java/eltex/service/UserService.java
@@ -0,0 +1,39 @@
+package eltex.service;
+
+import eltex.entity.Role;
+import eltex.entity.User;
+import eltex.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+
+/**
+ * Сервис класс для проведение опереций свзязанных с базой данных.
+ * Работает с транзакциями
+ *
+ * @author "Alexey Derevtsov"
+ * @version 1.0.0
+ */
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void registNewUser(User user) {
+        String code = passwordEncoder.encode(user.getPassword());
+        user.setPassword(code);
+        user.setConfirmPassword(code);
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+    }
+}

5ac320a007770877213fd536020b1f4d922acad8
AlexeyDer <hottelmich@gmail.com>
2020-01-16 16:42:48 +0700

    Add path to static

--- src/main/java/eltex/config/MvcConfig.java
+++ src/main/java/eltex/config/MvcConfig.java
@@ -2,6 +2,7 @@ package eltex.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/main").setViewName("main");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }

0781b99a71e1638da894b58d57011d0cb21cee4f
AlexeyDer <hottelmich@gmail.com>
2020-02-07 15:09:01 +0700

    Add sql files for clear test database and create users

--- src/test/resources/create-user-after.sql
+++ src/test/resources/create-user-after.sql
@@ -1,10 +1,2 @@
 DELETE FROM user_role;
 DELETE FROM user;
-
-INSERT INTO user(id, active, password, username) VALUES
-(1, true, '$2a$10$5hmRYxldck13npULWxZnSuggNCAxeQ0oJgxQjramHQiHhpUh/Gu4a', 'u')
-(2, true, '$2a$10$5hmRYxldck13npULWxZnSuggNCAxeQ0oJgxQjramHQiHhpUh/Gu4a', 'fred')
-
-INSERT INTO user_role(user_id, roles) VALUES
-(1, 'USER'), (1, 'ADMIN'),
-(2, 'USER');

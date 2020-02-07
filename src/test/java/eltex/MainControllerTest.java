641ec145ca525f2406b58c0f5d7770924e52ba55
AlexeyDer <hottelmich@gmail.com>
2020-02-07 15:09:29 +0700

    Add tests for main page

--- src/test/java/eltex/MainControllerTest.java
+++ src/test/java/eltex/MainControllerTest.java
@@ -2,27 +2,29 @@ package eltex;
 
 
 import eltex.controller.WebController;
-import eltex.repository.UserRepository;
-import eltex.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.xml.xpath.XPathExpressionException;
+
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.xpath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-
 /**
  * Класс для тестирования главнной страницы
  *
+ * Для этого класса нужно создать отдельную бд noSkypeTest
+ * на которой будет проходить тестироване
  * @author "Alexey Derevtsov"
  * @version 1.0.0
  *
@@ -30,21 +32,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser("usr")
+@TestPropertySource("/application-test.yml")
+@Sql(value = {"/create-user-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/create-user-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class MainControllerTest {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserService userService;
-
     @Autowired
     private WebController controller;
 
     @Autowired
     private MockMvc mockMvc;
-
+    /**
+     * Метод проверяет отбображение имени пользователя в панели навигации
+     * после входа в систему
+     */
     @Test
-    @WithMockUser("usr")
     public void mainPageTest() throws Exception {
         this.mockMvc.perform(get("/main"))
                 .andDo(print())
@@ -52,10 +54,15 @@ public class MainControllerTest {
                 .andExpect(MockMvcResultMatchers.xpath("//*[@id='name']").string("usr"));
     }
 
-//    @Test
-//    public void checkUser_whoHavntAccess() throws                   eption {
-//        this.mockMvc.perform(get("/get_users"))
-//                .andDo(print())
-//                .andExpect(status().isUnauthorized());
-//    }
+    /**
+     * Метод ожидающий определенное количество сообщений на странице при первом входе
+     * на страницу программы
+     */
+    @Test
+    public void checkUser_whoHavntAccess() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(MockMvcResultMatchers.xpath("/html/body/div[3]/div/div[2]/div/div[2]").nodeCount(0));
+    }
 }

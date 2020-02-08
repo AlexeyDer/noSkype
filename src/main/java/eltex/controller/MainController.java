60da6faeea7e0aea3bf305613c306d3e58ed595c
AlexeyDer <hottelmich@gmail.com>
2020-02-08 10:50:50 +0700

    Add get and post for start searching users:

---
+++ src/main/java/eltex/controller/MainController.java
@@ -0,0 +1,34 @@
+package eltex.controller;
+
+import eltex.entity.User;
+import eltex.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String getMainPage(Model model) {
+        return "main";
+    }
+
+    @PostMapping
+    public String searchUsers(@RequestParam String searchUsername) {
+       User user = userRepository.findByUsername(searchUsername);
+       if (user != null) {
+           return "main#" + searchUsername;
+       }
+        return "main";
+    }
+
+
+}

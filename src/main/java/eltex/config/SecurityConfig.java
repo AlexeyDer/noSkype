7910e85ca7ea20fee4054bc9078a8e0b21d19fe1
AlexeyDer <hottelmich@gmail.com>
2020-02-19 15:44:57 +0700

    Add path access to api

--- src/main/java/eltex/config/SecurityConfig.java
+++ src/main/java/eltex/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/", "/greeting", "/registration", "/errors/**", "/static/**").permitAll()
-                    .antMatchers("/get_users", "/get_users/{id}").permitAll()
+                    .antMatchers("/get_users", "/get_users/{id}", "/api/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()

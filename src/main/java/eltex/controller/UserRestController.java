c8e009e08fb1d2a35a358b544ff7f4c768dabc75
AlexeyDer <hottelmich@gmail.com>
2020-03-04 12:12:21 +0700

    Add loggin for UserRestController

--- src/main/java/eltex/controller/UserRestController.java
+++ src/main/java/eltex/controller/UserRestController.java
@@ -3,6 +3,7 @@ package eltex.controller;
 import eltex.entity.Role;
 import eltex.entity.User;
 import eltex.repository.UserRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 /**
- * Rest controller для получения пользователей в формате JSON
+ * Rest controller для получения или отправвления пользователей в формате JSON
  *
  * @author "Alexey Derevtsov"
  * @version 1.0.0
@@ -19,20 +20,26 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserRestController {
     /**
+     * Поле побявления переменной для логгирования
+     */
+    private static final Logger log = Logger.getLogger(MainController.class.getName());
+    /**
      * Поле для работы с пользователями из бд
      */
     @Autowired
     UserRepository userRepository;
     /**
      * Метод для отправки пользователя, по запросу "{/user/{id}}"
-     * @return данного пользователя или оповещает, что такого пользователя не найдено
+     * @return данного пользователя или оповещает, что такого пользователя не существует
      */
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> findByUserName(@PathVariable Long id) {
         User user = userRepository.findById(id);
         if (user == null) {
+            log.info("User not found by id: " + id.toString());
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        log.info("Get user request with id: " + id.toString());
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
     /**
@@ -50,7 +57,7 @@ public class UserRestController {
                 }
             }
         }
-
+        log.info("List users Received successfully");
         return userList;
     }
 

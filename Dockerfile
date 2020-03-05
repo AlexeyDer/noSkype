a071cf2e53ad1250ac0d2b961ca05980fb05fba9
ViktoriaGatz <ledneva.margo@gmail.com>
2020-03-05 08:24:54 +0700

    Change links

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:8
 ADD build/libs/spring-rest-service-0.1.0.jar spring-rest-service-0.1.0.jar
+COPY nginx_conf nginx_conf
+COPY src/main/resources/templates src/main/resources/templates/
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "/spring-rest-service-0.1.0.jar"]
+ENTRYPOINT ["java", "-jar", "spring-rest-service-0.1.0.jar"]
--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 sudo apt install nginx
-sudo rm /etc/nginx/sites-available/default
-cd /etc/nginx/sites-available/
-sudo ln -s ~YOU_NAME_COMP/noSkype/nginx_conf
-cd  ../sites-enabled/
-sudo rm default
-sudo ln -s /etc/nginx/sites-available/nginx_conf
+sudo rm /etc/nginx/sites-available/*
+sudo rm /etc/nginx/sites-enabled/*
+sudo ln -s "$PWD"/nginx_conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/nginx_conf /etc/nginx/sites-enabled/
 sudo service nginx restart

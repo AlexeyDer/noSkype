fe301fa2a7b43a40c5a9b0c2709066dfb57232c3
ViktoriaGatz <ledneva.margo@gmail.com>
2020-07-11 12:54:50 +0700

    revert

--- src/main/java/eltex/Application.java
+++ src/main/java/eltex/Application.java
@@ -1,6 +1,5 @@
 package eltex;
 
-import eltex.been.AppContext;
 import eltex.entity.Role;
 import eltex.entity.User;
 import eltex.repository.UserRepository;
@@ -10,14 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.MBeanExportConfiguration;
-import org.springframework.context.annotation.Scope;
 
 import java.util.Collections;
-import java.util.Random;
 
 /**
  * Класс для запуска программы
@@ -27,17 +21,38 @@ import java.util.Random;
  */
 @SpringBootApplication
 public class Application {
-
+    /**
+     * Поле подключения сервиса пользователя, с реализацией разной логики
+     */
+    @Autowired
+    private UserService userService;
+    /**
+     * Поле побявления переменной для логгирования
+     */
     private static final Logger log = Logger.getLogger(Application.class.getName());
+    /**
+     * Поля для взаимодействия программы с бд
+     */
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
-
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(AppContext.class);
-
         SpringApplication.run(Application.class, args);
         log.info("Start application!");
-
+    }
+    /**
+     * Метод автоматически создает пользователей в программе
+     */
+    @Bean
+    public CommandLineRunner demo(UserRepository userRepository) {
+        return (args) -> {
+            // Add user
+            userService.registNewUser(new User("u", "p"), false);
+            log.info("Create User");
+            // Add admin
+            userService.registNewUser(new User("admin", "p"), true);
+            log.info("Create Admin");
+        };
     }
 
 }
